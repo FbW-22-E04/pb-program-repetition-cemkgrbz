6da040c2ecc759346d2523343c427dfe447b79ce
Cem <cemkgrbz@gmail.com>
2022-07-12 15:51:13 +0200

    added solutions

---
+++ solution.js
@@ -0,0 +1,22 @@
+//1
+
+const i = 30;
+while (i < 15) {
+    console.log(i)
+};
+
+//2
+
+let i2=1,s=0; 
+while(i2<20) 
+ { 
+ s=s+i2; 
+ i2++;
+ console.log(s) 
+ }
+
+ //3
+
+ do {
+    console.log('The number is!', i)
+ } while (i < 20)
